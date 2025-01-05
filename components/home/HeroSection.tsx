e220f3e2a8d99e7097b6c1924ddebfaa00ecaa27
Ochaa <mushoddaqt@gmail.com>
2025-01-05 12:47:14 +0700

    ref: hero and h1 code refactor

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -1,55 +1,64 @@
-// components/HeroSection.jsx
-"use client";
-
 import LinkGreen from "./LinkGreen";
+
+const BackgroundVideo = () => {
+  return (
+    <video
+      autoPlay
+      muted
+      loop
+      playsInline
+      className="absolute left-0 top-0 h-full w-full object-cover transition-opacity duration-500"
+    >
+      <source src="/cbi-video-profile.mp4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
+
+const HeroTitle = () => {
+  return (
+    <div className="w-fit">
+      <h1 className="text-[#FDFDFD]">
+        Solusi <br /> Bioteknologi
+        <span className="font-normal"> Terintegrasi</span>
+        <span className="block xl:mt-2">
+          <span className="font-normal">untuk</span>
+          <span> Masa Depan</span>
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+const HeroDescription = () => {
+  return (
+    <div className="flex flex-col gap-6 lg:w-[380px] xl:w-[500px]">
+      <p className="text-[#FDFDFD]">
+        Temukan bagaimana solusi bioteknologi kami dapat mengatasi permasalahan
+        global di industri pertanian, peternakan, dan perikanan.
+      </p>
+
+      <LinkGreen href="/">Info Selengkapnya</LinkGreen>
+    </div>
+  );
+};
 
 const HeroSection = () => {
   return (
     <section className="relative h-screen w-full overflow-hidden">
       {/* Video Background */}
-      <video
-        autoPlay
-        muted
-        loop
-        playsInline
-        className={`absolute left-0 top-0 h-full w-full object-cover transition-opacity duration-500`}
-      >
-        <source src="/cbi-video-profile.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+      <BackgroundVideo />
 
       {/* Dark Overlay */}
       <div className="absolute inset-0 bg-black bg-opacity-50" />
 
       {/* Multi-step Blur Gradient */}
-      <div className="absolute bottom-0 left-0 right-0 h-96 backdrop-blur-lg [mask:linear-gradient(transparent,white,white)]"></div>
+      <div className="absolute bottom-0 left-0 right-0 h-96 backdrop-blur-lg [mask:linear-gradient(transparent,white,white)]" />
 
-      {/* Content Container */}
-      <div className="absolute bottom-0 left-1/2 z-10 mx-auto flex w-full max-w-7xl -translate-x-1/2 flex-col items-end gap-4 px-6 pb-24 text-white md:flex-row md:justify-between lg:bottom-6 lg:px-8 xl:px-0">
-        {/* Left Content (Welcome Text) */}
-        <div className="w-full">
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl">
-            <span className="font-bold">Solusi</span>
-            <span className="block xl:mt-2">
-              <span className="font-bold">Bioteknologi </span>
-              Terintegrasi
-            </span>
-            <span className="block xl:mt-2">
-              untuk <span className="font-bold">Masa Depan</span>
-            </span>
-          </h1>
-        </div>
-
-        {/* Right Content (Description and Button) */}
-        <div className="flex flex-col gap-6">
-          <p className="text-[#FDFDFD]">
-            Temukan bagaimana solusi bioteknologi kami dapat mengatasi
-            permasalahan global di industri pertanian, peternakan, dan
-            perikanan.
-          </p>
-
-          <LinkGreen href="/">Info Selengkapnya</LinkGreen>
-        </div>
+      {/* Main Content*/}
+      <div className="absolute bottom-0 left-1/2 z-10 mx-auto flex w-full max-w-7xl -translate-x-1/2 flex-col items-end gap-4 px-6 pb-24 md:flex-row md:justify-between lg:px-8 xl:px-0">
+        <HeroTitle />
+        <HeroDescription />
       </div>
     </section>
   );
