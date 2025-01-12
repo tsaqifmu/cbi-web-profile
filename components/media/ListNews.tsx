89a44bd2e0f9f095fea4cb715c300d0c9c816822
Ochaa <mushoddaqt@gmail.com>
2025-01-12 21:21:24 +0700

    feat: new blog detail page

--- components/media/ListNews.tsx
+++ components/media/ListNews.tsx
@@ -1,16 +1,7 @@
-import Image from "next/image";
-import React, { FC } from "react";
+import NewsCard from "./NewsCard";
 
 import ContainerSection from "@/components/layout/container";
-import ReadMoreButton from "./ReadMoreButton";
-import { ScrollArea, ScrollBar } from "../ui/scroll-area";
-
-interface NewsCardProps {
-  imageUrl: string;
-  title: string;
-  date: string;
-  description: string;
-}
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 const news = [
   {
@@ -54,37 +45,6 @@ const news = [
     imageUrl: "/img-news-1.png",
   },
 ];
-
-const NewsCard: FC<NewsCardProps> = ({
-  imageUrl,
-  title,
-  date,
-  description,
-}) => {
-  return (
-    <div className="w-[280px] flex-none snap-center lg:w-[306px] xl:w-[421px]">
-      <Image
-        src={imageUrl}
-        alt={`${title} image`}
-        width={421}
-        height={300}
-        className="rounded-lg object-cover object-center lg:rounded-3xl"
-      />
-
-      {/* News Content */}
-      <div className="mt-4 space-y-2">
-        <div className="flex items-center justify-between">
-          <h4 className="text-lg font-bold text-[#222222]">{title}</h4>
-          <p>{date}</p>
-        </div>
-        <p>{description}</p>
-      </div>
-
-      {/* Read More Button */}
-      <ReadMoreButton />
-    </div>
-  );
-};
 
 const ListNews = () => {
   return (
