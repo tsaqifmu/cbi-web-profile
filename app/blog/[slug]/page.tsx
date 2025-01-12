49a469a7c5028dc2f9db379ff5bbc9bd20dda829
Ochaa <mushoddaqt@gmail.com>
2025-01-12 21:21:24 +0700

    feat: new blog detail page

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import HeroSection from "@/components/media/article-detail/HeroSection";
+import ArticleDetail from "@/components/media/article-detail/ArticleDetail";
 
 const BlogDetail = () => {
-  return <section>BlogDetail</section>;
+  return (
+    <section>
+      <HeroSection />
+      <ArticleDetail />
+    </section>
+  );
 };
 
 export default BlogDetail;
