a2b1de984745f571bb95cbd6ff45202755a5effc
Ochaa <mushoddaqt@gmail.com>
2025-01-29 08:18:41 +0700

    feat(documents): create DocumentCard component for displaying document details and download functionality

--- components/document/DocumentBrochure.tsx
+++ components/document/DocumentBrochure.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { FC, ReactNode, useState } from "react";
-import Image from "next/image";
 
 import { cn } from "@/lib/utils";
+import { downloadPDF } from "@/utils/downloadPdf";
 
 import { Button } from "@/components/ui/button";
 import ContainerSection from "@/components/layout/container";
-import { downloadPDF } from "@/utils/downloadPdf";
+import { DocumentCard } from "@/components/document/DocumentCard";
 
 enum DocumentType {
   BROCHURE = "brochure",
@@ -84,43 +84,6 @@ const TabButton: FC<TabButtonProps> = ({ isActive, onClick, children }) => (
   >
     {children}
   </Button>
-);
-
-interface DocumentCardProps {
-  document: Document;
-  onDownload: (id: number) => void;
-  isDownloading: boolean;
-}
-
-const DocumentCard: FC<DocumentCardProps> = ({
-  document,
-  onDownload,
-  isDownloading,
-}) => (
-  <div className="flex flex-col gap-6 rounded-lg bg-white p-4 shadow lg:flex-row lg:items-center">
-    <Image
-      src="/placeholder-image.png"
-      alt={`${document.title} thumbnail`}
-      width={144}
-      height={144}
-      className="h-36 w-full rounded-lg object-cover object-center lg:w-36"
-    />
-
-    <div className="flex-grow">
-      <h5 className="text-xl font-bold text-[#222222] lg:text-2xl">
-        {document.title}
-      </h5>
-      <p>{document.description}</p>
-    </div>
-
-    <Button
-      className={`w-fit bg-[#009933] hover:bg-[#009933]/70`}
-      onClick={() => onDownload(document.id)}
-      disabled={isDownloading}
-    >
-      {isDownloading ? "Mengunduh..." : "Unduh"}
-    </Button>
-  </div>
 );
 
 const DocumentBrochure = () => {
