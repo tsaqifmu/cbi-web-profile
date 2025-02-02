3c4c1efd1638f716aa78eec8d7e0ea357416d9e0
Ochaa <mushoddaqt@gmail.com>
2025-02-02 10:59:14 +0700

    refactor: remove unused type definitions from utils/types.ts

--- utils/types.ts
+++ utils/types.ts
@@ -2,113 +2,6 @@ import { BannerContactSection } from "@/types/responseTypes/dashboard/bannerCont
 import { Headline } from "@/types/responseTypes/dashboard/headline";
 import { WhySection } from "@/types/responseTypes/dashboard/whySection";
 
-// export interface ProductService {
-//   id: number;
-//   title: string;
-//   description: string;
-//   ctaText: string;
-//   products: Product[];
-// }
-
-// export interface OurImpact {
-//   id: number;
-//   title: string;
-//   description: string;
-//   product1image: Image;
-//   product1bgimage: Image;
-//   product2image: Image;
-//   product2bgimage: Image;
-// }
-
-// export interface BannerContact {
-//   id: number;
-//   title: string;
-//   ctaText: string;
-//   image: Image;
-// }
-
-// export interface Product {
-//   id: number;
-//   title: string;
-//   description: string;
-//   image: Image;
-//   url: string;
-//   color1: string;
-//   color2: string;
-//   documentId: string;
-//   createdAt: string;
-//   updatedAt: string;
-//   publishedAt: string;
-// }
-
-// export interface Data {
-//   id: number;
-//   documentId: string;
-//   createdAt: string;
-//   updatedAt: string;
-//   publishedAt: string;
-
-//   headline: Headline;
-//   whySection: WhySection;
-//   productService: ProductService;
-//   ourImpact: OurImpact;
-//   latestNews: LatestNews;
-//   bannerContact: BannerContact;
-// }
-// interface ContentChild {
-//   type: string;
-//   text: string;
-//   bold?: boolean;
-// }
-// interface ImageType {
-//   name: string;
-//   alternativeText: string;
-//   url: string;
-//   caption: string | null;
-//   width: number;
-//   height: number;
-//   formats: {
-//     thumbnail: ImageFormat;
-//   };
-//   hash: string;
-//   ext: string;
-//   mime: string;
-//   size: number;
-//   previewUrl: string | null;
-//   provider: string;
-//   provider_metadata: any;
-//   createdAt: string;
-//   updatedAt: string;
-// }
-// interface ContentBlock {
-//   type: string;
-//   children: ContentChild[];
-//   level?: number;
-//   image?: ImageType;
-// }
-// export interface Article {
-//   id: number;
-//   documentId: string;
-//   title: string;
-//   shortDescription: string;
-//   content: ContentBlock[];
-//   createdAt: string;
-//   updatedAt: string;
-//   publishedAt: string;
-//   slug: string;
-//   type?: string; // Opsional karena hanya ada di beberapa artikel
-// }
-// export interface LatestNews {
-//   id: number;
-//   blogs: Article[];
-//   news: Article[];
-// }
-
-// export interface DashboardResponse {
-//   data: Data;
-//   meta: Record<string, never>;
-// }
-
 export interface ImageFormat {
   name: string;
   hash: string;
@@ -149,13 +42,6 @@ export interface Image {
   publishedAt: string;
 }
 
-// export interface WhySection {
-//   id: number;
-//   title: string;
-//   description: string;
-//   image: Image;
-// }
-
 interface Product {
   id: number;
   documentId: string;
@@ -194,13 +80,6 @@ interface ServicesSection {
   description: string;
   services: Service[];
 }
-
-// export interface BannerContactSection {
-//   id: number;
-//   title: string;
-//   ctaText: string;
-//   image: Image;
-// }
 
 export interface ProductAndServiceData {
   id: number;
@@ -262,13 +141,6 @@ export interface ProductCategory {
   publishedAt: string;
   product_items: ProductItem[];
 }
-
-// export interface BannerContactSection {
-//   id: number;
-//   title: string;
-//   ctaText: string;
-//   image: Image;
-// }
 
 export interface AgricultureData {
   id: number;
