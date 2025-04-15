58721d0311fc28156fa31c5d925844f22345bd99
Verry <verryrwibawa@gmail.com>
2025-04-15 08:48:34 +0700

    feat(*): migrate backend to https

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "213.210.21.45",
-        port: "9337",
+        protocol: "https",
+        hostname: "cbi-backend.my.id",
       },
     ],
   },
